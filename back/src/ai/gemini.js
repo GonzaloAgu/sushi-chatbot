423118947fbac67798a063de1acc76d284e22c68
GonzaloAgu <gonzalo.ag88@gmail.com>
2025-01-08 22:47:01 -0300

    codigo repe

--- back/src/ai/gemini.js
+++ back/src/ai/gemini.js
@@ -7,28 +7,25 @@ let prompts = readPrompts();
 const apiKey = process.env.GEMINI_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
-export const askGemini = async (msg) => {
-  const model = genAI.getGenerativeModel({
+const getModel = schema => {
+  return genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
     systemInstruction: prompts.default,
     generationConfig: {
       temperature: 0.1,
       responseMimeType: "application/json",
-      responseSchema: GeminiResponseSchemas.default
+      responseSchema: schema
     }
 
-  });
+  })
+}
+
+export const askGemini = async (msg) => {
+  const model = getModel(GeminiResponseSchemas.default)
   const result = await model.generateContent(msg);
   return JSON.parse(result.response.text());
 };
 
-export const productsSelected = (userMsg) => {
-  const generationConfig = {
-    temperature: 0,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "application/json",
-    responseSchema: GeminiResponseSchemas.productList,
-  };
+export const productsSelected = (msg) => {
+  const model = getModel(GeminiResponseSchemas.productList)
 };
